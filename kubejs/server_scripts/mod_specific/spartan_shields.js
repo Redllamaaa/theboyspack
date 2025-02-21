918d089155202b4799e36a8a6596af81d4011017
Redllamaaa <masterhanlon867@gmail.com>
2025-02-21 02:49:58 +0000

    [KubeJS] Setup Pre-configuration for the disabling of some Spartan Shields

---
+++ kubejs/server_scripts/mod_specific/spartan_shields.js
@@ -0,0 +1,16 @@
+// priority: 0
+
+/* ServerEvents.recipes(spartanshields => { 
+
+	// Removal
+	spartanshields.remove({ output: 'spartanshields:enderium_tower_shield'});
+    spartanshields.remove({ output: 'spartanshields:enderium_basic_shield'});
+    spartanshields.remove({ output: 'spartanshields:lumium_tower_shield'});
+    spartanshields.remove({ output: 'spartanshields:lumium_basic_shield'});
+    spartanshields.remove({ output: 'spartanshields:signalum_tower_shield'});
+    spartanshields.remove({ output: 'spartanshields:signalum_basic_shield'});
+  })
+
+
+
+console.info('[KubeJS] Spartan Shields kubejs loaded.') */
